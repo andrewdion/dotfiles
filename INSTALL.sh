85453350f22cf7af625f390171bd6b633bd53257
Andrew Dion <andrew.r.dion@gmail.com>
2016-02-11 00:16:04 -0500

    moved non-essential files (status.sh, lock screen stuff) out of home dir; removed slim

--- INSTALL.sh
+++ INSTALL.sh
@@ -14,10 +14,8 @@
 # feh
 # scrot
 # imagemagick
-# epdfview?
 # alsa-lib
 # alsa-utils
-# slim
 # screenfetch
 # fonts (terminus-font, ttf-dejavu, ttf-droid, ttf-inconsolata)
 # xbindkeys
@@ -33,9 +31,7 @@ ln -sf ${dotfiles}/bashrc .bashrc
 ln -sf ${dotfiles}/gitconfig .gitconfig
 ln -sf ${dotfiles}/spectrwm.conf .spectrwm.conf
 ln -sf ${dotfiles}/spectrwm_us.conf .spectrwm_us.conf
-ln -sf ${dotfiles}/status.sh .status.sh
 ln -sf ${dotfiles}/vimrc .vimrc
 ln -sf ${dotfiles}/xbindkeysrc .xbindkeysrc
 ln -sf ${dotfiles}/xinitrc .xinitrc
 ln -sf ${dotfiles}/Xresources .Xresources
-sudo ln -sf ${dotfiles}/slim.conf /etc/slim.conf
