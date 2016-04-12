e14119eda5145c03c1e384e24c263b707ada6184
Andrew Dion <andrew.r.dion@gmail.com>
2016-04-11 20:15:38 -0400

    does this work?

--- publish_dir.sh
+++ publish_dir.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+# $dir must not include trailing slash!
 dir=$1
 
-# $dir must not include trailing slash!!
-
+# can be either "local" or "remote"
 location=$2
 
 if [ "$location" == "local" ]; then
