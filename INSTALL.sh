b76ee369678dddbe8cc0d6676f740d98a6cfc281
Andrew Dion <andrew.r.dion@gmail.com>
2016-05-25 13:21:04 -0400

    removing httpd; updating install script

--- INSTALL.sh
+++ INSTALL.sh
@@ -7,6 +7,7 @@
 # xorg-xrdb
 # spectrwm
 # urxvt 
+# vim
 # git
 # chromium
 # wget
@@ -16,14 +17,20 @@
 # alsa-lib
 # alsa-utils
 # screenfetch
-# fonts (terminus-font, ttf-dejavu, ttf-droid, ttf-inconsolata)
+# fonts:
+#  terminus-font
+#  ttf-dejavu
+#  ttf-droid
+#  ttf-inconsolata
 # xbindkeys
 # apache
 # python
-# mod_wsgi
-# lemonbar-xft-git (aur)
 # dmenu2 (aur)
-# i3lock-color (aur)
+# lemonbar-xft-git (aur)
+# i3lock-color-git (aur)
+# google-chrome (aur)
+# ttf-font-awesome (aur)
+# sublime-text (aur)
 
 home=/home/adion
 dotfiles=${home}/dotfiles
@@ -33,6 +40,7 @@ cd $home
 ln -sf ${dotfiles}/asoundrc .asoundrc
 ln -sf ${dotfiles}/bash_profile .bash_profile
 ln -sf ${dotfiles}/bashrc .bashrc
+ln -sf ${dotfiles}/fehbg .fehbg
 ln -sf ${dotfiles}/gitconfig .gitconfig
 ln -sf ${dotfiles}/spectrwm.conf .spectrwm.conf
 ln -sf ${dotfiles}/spectrwm_us.conf .spectrwm_us.conf
@@ -40,4 +48,3 @@ ln -sf ${dotfiles}/vimrc .vimrc
 ln -sf ${dotfiles}/xbindkeysrc .xbindkeysrc
 ln -sf ${dotfiles}/xinitrc .xinitrc
 ln -sf ${dotfiles}/Xresources .Xresources
-sudo ln -sf ${dotfiles}/httpd.conf /etc/httpd/conf/httpd.conf
