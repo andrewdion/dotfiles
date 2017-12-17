502ad314b9be07581d4f21b2ac300c794212e157
Andrew Dion <andrew.r.dion@gmail.com>
2017-12-17 15:59:07 -0500

    a

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,42 +1,49 @@
 #!/bin/bash
 
 sudo pacman -S xf86-video-ati \
-                xorg-server \
-                xorg-xinit \
-                xorg-xrdb \
-                xorg-xrandr \
-                xbindkeys \
-                xvkbd \
-                rxvt-unicode \
-                autocutsel \
-                tmux \
-                wget \
-                feh \
-                i3lock \
-                scrot \
-                imagemagick \
-                chromium \
-                alsa-lib \
-                alsa-utils \
-                screenfetch \
-                ttf-inconsolata \
-                unzip \
-                openssh \
-                bluez \
-                bluez-utils \
-                python \
-                python2 \
-                python2-virtualenv \
-                vagrant \
-                virtualbox \
-                rsync \
-                nmap \
-                net-tools \
-                cups
-#                terminus-font \
-#                ttf-dejavu \ 
-#                ttf-droid \
-#                firefox \
+               xorg-server \
+               xorg-xinit \
+               xorg-xrdb \
+               xorg-xrandr \
+               xbindkeys \
+               xvkbd \
+               openssh \
+               spectrwm \
+               rxvt-unicode \
+               autocutsel \
+               tmux \
+               wget \
+               i3lock \
+               scrot \
+               imagemagick \
+               alsa-lib \
+               alsa-utils \
+               screenfetch \
+               ttf-inconsolata \
+               ttf-droid \
+               zip \
+               unzip \
+               python2-virtualenv \
+               python2-setuptools \
+               rsync \
+               nmap \
+               net-tools \
+               openconnect \
+               htop
+#               feh \
+#               bluez \
+#               bluez-utils \
+#               cups
+#               vagrant \
+#               virtualbox \
+#               terminus-font \
+
+# some nice fonts:
+# ttf-liberation
+# ttf-roboto
+# ttf-dejavu
+# ttf-chroscore
+# noto-fonts
 
 home=/home/adion
 
@@ -46,7 +53,8 @@ cd $aur
 
 aur_www="https://aur.archlinux.org/cgit/aur.git/snapshot/"
 
-for pkg in dmenu2; do
+for pkg in dmenu2 \
+           google-chrome; do
 #           i3-gaps-git; do
 #           hipchat; do
     wget ${aur_www}/${pkg}.tar.gz
@@ -64,37 +72,20 @@ ln -sf ${dotfiles}/bash_profile .bash_profile
 ln -sf ${dotfiles}/bashrc .bashrc
 ln -sf ${dotfiles}/fehbg .fehbg
 ln -sf ${dotfiles}/gitconfig .gitconfig
+ln -sf ${dotfiles}/spectrwm.conf .spectrwm.conf
+ln -sf ${dotfiles}/spectrwm_us.conf .spectrwm_us.conf
 ln -sf ${dotfiles}/tmux.conf .tmux.conf
 ln -sf ${dotfiles}/vimrc .vimrc
 ln -sf ${dotfiles}/xbindkeysrc .xbindkeysrc
 ln -sf ${dotfiles}/xinitrc .xinitrc
 ln -sf ${dotfiles}/Xresources .Xresources
 
-sudo systemctl start bluetooth
-sudo systemctl enable bluetooth
-
 # auto login
 sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
 # for some reason this doesn't like symlinks
-sudo cp ${doftiles}/autologin /etc/systemd/system/getty@tty1.service.d/override.conf
+sudo cp ${dotfiles}/autologin /etc/systemd/system/getty@tty1.service.d/override.conf
 
-sudo ln -sf ${dotfiles}/mouse-sensitivity /etc/X11/xorg.conf.d/50-mouse-acceleration.conf
+#sudo ln -sf ${dotfiles}/mouse-sensitivity /etc/X11/xorg.conf.d/50-mouse-acceleration.conf
 
+# ?
 sudo ln -sf ${dotfiles}/logind.conf /etc/systemd/logind.conf
-
-# not using i3 anymore
-#i3=${home}/.config/i3
-#mkdir -p $i3
-#ln -sf ${dotfiles}/i3 ${i3}/config
-
-# make python2 the default
-#sudo ln -s /usr/bin/python2 /usr/bin/python
-# 2017-11-13
-# global python should be the latest.
-# local virtualenv pythons can be whatever version.
-
-# pip packages to install
-#sudo pip install -upgrade ansible awscli boto virtualenv
-# 2017-11-13:
-# no need for a global pip.
-# each virtualenv can have its own set of packages.
