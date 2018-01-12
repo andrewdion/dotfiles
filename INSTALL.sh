2f8eb1f61de3aec5070d6017dce3f44f80eadf3e
Andrew Dion <andrew_dion@hms.harvard.edu>
2018-01-11 19:24:59 -0500

    important stuff

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,9 +4,7 @@ sudo pacman -S xf86-video-ati \
                xorg-server \
                xorg-xinit \
                xorg-xrdb \
-               xorg-xrandr \
                xbindkeys \
-               xvkbd \
                openssh \
                spectrwm \
                rxvt-unicode \
@@ -30,6 +28,8 @@ sudo pacman -S xf86-video-ati \
                net-tools \
                openconnect \
                htop
+#               xvkbd \
+#               xorg-xrandr \
 #               feh \
 #               bluez \
 #               bluez-utils \
@@ -53,8 +53,8 @@ cd $aur
 
 aur_www="https://aur.archlinux.org/cgit/aur.git/snapshot/"
 
-for pkg in dmenu2 \
-           google-chrome; do
+for pkg in google-chrome; do
+#          dmenu2 \
 #           i3-gaps-git; do
 #           hipchat; do
     wget ${aur_www}/${pkg}.tar.gz
@@ -70,22 +70,22 @@ cd $home
 ln -sf ${dotfiles}/asoundrc .asoundrc
 ln -sf ${dotfiles}/bash_profile .bash_profile
 ln -sf ${dotfiles}/bashrc .bashrc
-ln -sf ${dotfiles}/fehbg .fehbg
 ln -sf ${dotfiles}/gitconfig .gitconfig
 ln -sf ${dotfiles}/spectrwm.conf .spectrwm.conf
 ln -sf ${dotfiles}/spectrwm_us.conf .spectrwm_us.conf
 ln -sf ${dotfiles}/tmux.conf .tmux.conf
 ln -sf ${dotfiles}/vimrc .vimrc
+ln -sf ${dotfiles}/Xresources .Xresources
 ln -sf ${dotfiles}/xbindkeysrc .xbindkeysrc
 ln -sf ${dotfiles}/xinitrc .xinitrc
-ln -sf ${dotfiles}/Xresources .Xresources
+#ln -sf ${dotfiles}/fehbg .fehbg
 
 # auto login
 sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
 # for some reason this doesn't like symlinks
 sudo cp ${dotfiles}/autologin /etc/systemd/system/getty@tty1.service.d/override.conf
 
-#sudo ln -sf ${dotfiles}/mouse-sensitivity /etc/X11/xorg.conf.d/50-mouse-acceleration.conf
+sudo ln -sf ${dotfiles}/mouse-sensitivity /etc/X11/xorg.conf.d/50-mouse-acceleration.conf
 
 # ?
 sudo ln -sf ${dotfiles}/logind.conf /etc/systemd/logind.conf
