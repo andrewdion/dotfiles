9abff32c2dc93d731ac50c704b3ca8650e45861c
Andrew Dion <andrew.r.dion@gmail.com>
2018-02-10 12:55:16 -0500

    sknaonuehtd

--- INSTALL.sh
+++ INSTALL.sh
@@ -47,7 +47,7 @@ sudo pacman -S xf86-video-ati \
 
 home=/home/adion
 
-aur=${home}/aur
+aur=$home/aur
 mkdir -p $aur
 cd $aur
 
@@ -62,30 +62,33 @@ for pkg in google-chrome; do
     cd ..
 done
 
-dotfiles=${home}/dotfiles
+dotfiles=$home/dotfiles
+cd $dotfiles
+git config user.email "andrew.r.dion@gmail.com"
+
 cd $home
 
-ln -sf ${dotfiles}/asoundrc .asoundrc
-ln -sf ${dotfiles}/bash_profile .bash_profile
-ln -sf ${dotfiles}/bashrc .bashrc
-ln -sf ${dotfiles}/gitconfig .gitconfig
-ln -sf ${dotfiles}/spectrwm.conf .spectrwm.conf
-ln -sf ${dotfiles}/spectrwm_us.conf .spectrwm_us.conf
-ln -sf ${dotfiles}/tmux.conf .tmux.conf
-ln -sf ${dotfiles}/vimrc .vimrc
-ln -sf ${dotfiles}/Xresources .Xresources
-ln -sf ${dotfiles}/xinitrc .xinitrc
-#ln -sf ${dotfiles}/xbindkeysrc .xbindkeysrc
-#ln -sf ${dotfiles}/fehbg .fehbg
+cp -r $dotfiles/bin bin
+
+ln -sf $dotfiles/asoundrc .asoundrc
+ln -sf $dotfiles/bash_profile .bash_profile
+ln -sf $dotfiles/bashrc .bashrc
+ln -sf $dotfiles/gitconfig .gitconfig
+ln -sf $dotfiles/spectrwm.conf .spectrwm.conf
+ln -sf $dotfiles/spectrwm_us.conf .spectrwm_us.conf
+ln -sf $dotfiles/tmux.conf .tmux.conf
+ln -sf $dotfiles/vimrc .vimrc
+ln -sf $dotfiles/Xresources .Xresources
+ln -sf $dotfiles/xinitrc .xinitrc
+#ln -sf $dotfiles/xbindkeysrc .xbindkeysrc
+#ln -sf $dotfiles/fehbg .fehbg
 
 # auto login
 sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
 # for some reason this doesn't like symlinks
-sudo cp ${dotfiles}/autologin /etc/systemd/system/getty@tty1.service.d/override.conf
+sudo cp $dotfiles/autologin /etc/systemd/system/getty@tty1.service.d/override.conf
 
-#sudo ln -sf ${dotfiles}/mouse-sensitivity /etc/X11/xorg.conf.d/50-mouse-acceleration.conf
+#sudo ln -sf $dotfiles/mouse-sensitivity /etc/X11/xorg.conf.d/50-mouse-acceleration.conf
 
 # ?
-sudo ln -sf ${dotfiles}/logind.conf /etc/systemd/logind.conf
-
-cp -r ${dotfiles}/bin bin
+sudo ln -sf $dotfiles/logind.conf /etc/systemd/logind.conf
