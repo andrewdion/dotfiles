3e6c3ab644512fe7e51d9003ce4f496959388a55
Andrew Dion <andrew_dion@hms.harvard.edu>
2021-08-03 17:31:47 -0400

    updates for INSTALL on centos7 vm

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,114 +1,79 @@
 #!/bin/bash
 
-sudo pacman -S xf86-video-ati \
-               xorg-server \
-               xorg-xinit \
-               xorg-xrdb \
-               xorg-xrandr \
-               xbindkeys \
-               spectrwm \
-               rxvt-unicode \
-               autocutsel \
-               tmux \
-               i3lock \
-               scrot \
-               imagemagick \
-               screenfetch \
-               zip \
-               unzip \
-               openssh \
-               bluez \
-               bluez-utils \
-               python2-virtualenv \
-               python2-setuptools \
-               vagrant \
-               virtualbox \
-               rsync \
-               nmap \
-               net-tools \
-               feh \
-               alsa-lib \
-               alsa-utils \
-               ttf-inconsolata \
-               ttf-croscore
+#sudo pacman -S xf86-video-ati \
+#               xorg-server \
+#               xorg-xinit \
+#               xorg-xrdb \
+#               xorg-xrandr \
+#               xbindkeys \
+#               spectrwm \
+#               rxvt-unicode \
+#               autocutsel \
+#               tmux \
+#               i3lock \
+#               scrot \
+#               imagemagick \
+#               screenfetch \
+#               zip \
+#               unzip \
+#               openssh \
+#               bluez \
+#               bluez-utils \
+#               python2-virtualenv \
+#               python2-setuptools \
+#               vagrant \
+#               virtualbox \
+#               rsync \
+#               nmap \
+#               net-tools \
+#               feh \
+#               alsa-lib \
+#               alsa-utils \
+#               ttf-inconsolata \
+#               ttf-croscore
 
-# other fun fonts:
-# ttf-dejavu
-# ttf-liberation
-# ttf-droid
-# noto-fonts
-
-home=/home/adion
-
-aur=${home}/aur
-mkdir -p $aur
-cd $aur
-
-aur_www="https://aur.archlinux.org/cgit/aur.git/snapshot/"
-
-for pkg in google-chrome \
-           xvkbd \
-           apg; do
-    wget ${aur_www}/${pkg}.tar.gz
-    tar xf ${pkg}.tar.gz
-    cd $pkg
-    makepkg -sri
-    cd ..
-done
-
-git clone https://github.com/tmux-plugins/tmux-resurrect
-
+sudo yum install automake \       # tmux dependencies
+                 libevent \       #
+                 libevent-devel \ #
+                 ncurses \        #
+                 ncurses-devel \  #
+                 gcc \            #
+                 make \           #
+                 bison \          #
+                 pkg-config       #
+ 
+HOME=/home/adion
+BUILD=$HOME/build
+ 
+# TMUX
+######
+cd $BUILD
+git clone https://github.com/tmux/tmux.git
+cd tmux
+sh autogen.sh
+./configure
+make && sudo make install
+ 
+#git clone https://github.com/tmux-plugins/tmux-resurrect
+ 
 dotfiles=${home}/dotfiles
 cd $home
-
+ 
 ln -sf ${dotfiles}/bash_profile .bash_profile
 ln -sf ${dotfiles}/bashrc .bashrc
 ln -sf ${dotfiles}/gitconfig .gitconfig
-ln -sf ${dotfiles}/spectrwm.conf .spectrwm.conf
-ln -sf ${dotfiles}/spectrwm_us.conf .spectrwm_us.conf
 ln -sf ${dotfiles}/tmux.conf .tmux.conf
 ln -sf ${dotfiles}/vimrc .vimrc
-ln -sf ${dotfiles}/Xresources .Xresources
-ln -sf ${dotfiles}/xbindkeysrc .xbindkeysrc
-ln -sf ${dotfiles}/xinitrc .xinitrc
-ln -sf ${dotfiles}/asoundrc.E1 .asoundrc
-#ln -sf ${dotfiles}/fehbg .fehbg
-
-# doesn't like symlinks
-sudo cp ${dotfiles}/bluetooth.conf /etc/bluetooth/main.conf
-sudo systemctl start bluetooth
-sudo systemctl enable bluetooth
-
-sudo systemctl start sshd.socket
-sudo systemctl enable sshd.socket
-
-# must already have internet connection to clone this repo
-#sudo systemctl start dhcpcd@eno1
-sudo systemctl enable dhcpcd@eno1
-
-# auto login; doesn't like symlinks
-sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
-sudo cp ${dotfiles}/autologin.conf /etc/systemd/system/getty@tty1.service.d/override.conf
-
-sudo ln -sf ${dotfiles}/sudoers.conf /etc/sudoers.d/adion
-
-sudo ln -sf ${dotfiles}/mouse-sensitivity.conf /etc/X11/xorg.conf.d/50-mouse-acceleration.conf
-
-# lid switch & power key actions... doesn't like symlinks
-sudo cp ${dotfiles}/logind.conf /etc/systemd
-
-# disable pc speaker
-sudo ln -sf ${dotfiles}/nobeep.conf /etc/modprobe.d/nobeep.conf
-
-# fonts
-# https://www.reddit.com/r/archlinux/comments/5r5ep8/make_your_arch_fonts_beautiful_easily
-# $ fc-list : file | sort
-sudo ln -s /etc/fonts/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d
-sudo ln -s /etc/fonts/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
-sudo ln -s /etc/fonts/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
-# vim /etc/profile.d/freetype2.sh
-# uncomment 'export FREETYPE_PROPERTIES' line
-# there is more, but this seems to be sufficient
+ 
+# # fonts
+# # https://www.reddit.com/r/archlinux/comments/5r5ep8/make_your_arch_fonts_beautiful_easily
+# # $ fc-list : file | sort
+# sudo ln -s /etc/fonts/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d
+# sudo ln -s /etc/fonts/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
+# sudo ln -s /etc/fonts/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
+# # vim /etc/profile.d/freetype2.sh
+# # uncomment 'export FREETYPE_PROPERTIES' line
+# # there is more, but this seems to be sufficient
 
 git config user.name "Andrew Dion"
 git config user.email "andrew.r.dion@gmail.com"
